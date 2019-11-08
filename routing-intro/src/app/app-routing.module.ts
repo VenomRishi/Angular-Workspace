83518b3a699a05a97f18934137a0707310dcefb3
VenomRishi <mhatrerishikesh@gmail.com>
2019-11-08 19:03:58 +0530

    routing project is added

---
+++ routing-intro/src/app/app-routing.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { Route, RouterModule } from '@angular/router';
+import { HomeComponent} from './home/home.component';
+import { SettingsComponent} from './settings/settings.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { SettingsProfileComponent } from './settings-profile/settings-profile.component';
+import { SettingsContactComponent } from './settings-contact/settings-contact.component';
+
+
+
+const routes: Route[] = [
+  {path: '', redirectTo: '/home', pathMatch: 'full' },
+  {path: 'home', component: HomeComponent },
+  {path: 'settings', component: SettingsComponent, children: [
+    {path: 'profile', component: SettingsProfileComponent },
+    {path: 'contact', component: SettingsContactComponent }
+  ] },
+  {path: '**', component: PageNotFoundComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
